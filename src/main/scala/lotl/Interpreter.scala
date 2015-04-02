72f74dd8fd2330583193fc0be6d4a81ef3a13de6
Alexander Konovalov <alex.knvl@gmail.com>
2015-04-01 20:30:37 -0400

    Return the function name from DEFUN.

--- src/main/scala/lotl/Interpreter.scala
+++ src/main/scala/lotl/Interpreter.scala
@@ -85,7 +85,7 @@ trait BasicForms extends InterpreterTypes {
           eval(body)(env.withLocals(paramNames.zip(params)))
         }
       }
-      (env.withFunction(name -> func), TrueAtom).right
+      (env.withFunction(name -> func), name).right
     }
   }
 
